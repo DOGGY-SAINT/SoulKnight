a499c4c90f753716b9b67be831f6cf2681776ffe
jackie1950057 <2821166215@qq.com>
2020-05-27 19:04:40 +0800

    。。。
    。。。

--- Resources/newmap.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="{98883E80-14DE-C53C-2C7D-D8085161EDA5}" tilewidth="16" tileheight="16" tilecount="2520" columns="70">
- <image source="{98883E80-14DE-C53C-2C7D-D8085161EDA5}.jpg" width="1120" height="576"/>
-</tileset>
